eb1d047ce67aede6bf7f603f9d538abde2b7e8aa
l1ghttm <ps4j@bk.ru>
2024-12-12 10:10:08 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 with open('text.txt', 'r', encoding='utf-8') as file: # открытие файла 
     spis = file.read() #присваивание переменной к файлу
 
-words= spis.split() #присваивание переменной через str
+words = spis.split() #присваивание переменной через spis
 
 length=len(words) #подчет переменной word
 print('Количество слов равно:',length) #вывод
